e1ccbb70605f93ee97dfdaef2d75bcd1be939cc0
tony501com <tony501@139.com>
2025-03-07 08:13:58 +0800

    更新 Cloudflare Pages D1 CRUD 代码

---
+++ functions/t1.js
@@ -0,0 +1,14 @@
+import cheerio from 'cheerio';
+
+export async function onRequest(context) {
+  const htmlString = `<div><h1>Hello, World!</h1><p>This is a paragraph.</p></div>`;
+  const $ = cheerio.load(htmlString);
+
+  // 使用类似 jQuery 的语法操作 DOM
+  const h1Text = $('h1').text();
+  const pText = $('p').text();
+
+  return new Response(`h1: ${h1Text}, p: ${pText}`, {
+    headers: { 'Content-Type': 'text/plain' },
+  });
+}
