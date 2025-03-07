37995a534614d534bd880eab7ef02b3b8628dfc9
tony501com <tony501@139.com>
2025-03-07 16:21:11 +0800

    更新 Cloudflare Pages D1 CRUD 代码

---
+++ functions/t3.js
@@ -0,0 +1,24 @@
+const cheerio = require('cheerio');
+
+// 假设这是你的 HTML 字符串
+const html = `
+<div class="xcblog-v2ray-box">
+  <p>https://node.freeclashnode.com/uploads/2025/03/0-20250303.txt</p>
+  <p>https://node.freeclashnode.com/uploads/2025/03/1-20250303.txt</p>
+  <p>https://node.freeclashnode.com/uploads/2025/03/2-20250303.txt</p>
+  <p>https://node.freeclashnode.com/uploads/2025/03/3-20250303.txt</p>
+  <p>https://node.freeclashnode.com/uploads/2025/03/4-20250303.txt</p>
+</div>
+`;
+
+// 加载 HTML
+const $ = cheerio.load(html);
+
+// 提取所有 <p> 标签的内容
+const pContents = [];
+$('div.xcblog-v2ray-box p').each((index, element) => {
+  pContents.push($(element).text());
+});
+
+// 输出结果
+console.log(pContents);
