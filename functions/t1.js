789b906e42ad6e5f63d77b56094b03fc0316d1b6
tony501com <115136071+tony501com@users.noreply.github.com>
2025-03-16 05:06:15 +0800

    更新 Cloudflare Pages

--- functions/t1.js
+++ functions/t1.js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
-  console.log('Starting fetch request to https://mianfeiclash.github.io/newly-discovered-nodes/index.html?date=2025-3-14');
   try {
-    const response = await fetch('https://mianfeiclash.github.io/newly-discovered-nodes/index.html?date=2025-3-14', {
+    const response = await fetch('https://node.freeclashnode.com/uploads/2025/03/2-20250314.txt', {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
